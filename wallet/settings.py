641900017eb64513a5bcaeee18ab7000bd3a33b8
David O'Connor <the_alchemist@fastmail.com>
2024-03-08 23:07:23 -0500

    Use production when deployed, always

--- wallet/settings.py
+++ wallet/settings.py
@@ -38,8 +38,8 @@ PLAID_MODE = PlaidMode.SANDBOX
 # PLAID_MODE = PlaidMode.PRODUCTION
 
 if DEPLOYED:
+    PLAID_MODE = PlaidMode.PRODUCTION
     DEBUG = False
-    DEBUG = True  # todo temp!
     # SECURITY WARNING: keep the secret key used in production secret!
     SECRET_KEY = os.environ["SECRET_KEY"]
     PLAID_CLIENT_ID = os.environ["PLAID_CLIENT_ID"]
