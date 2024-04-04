0d41c862936372c455aa31436b1c96508ef3c92a
David O'Connor <the_alchemist@fastmail.com>
2024-04-03 21:52:45 -0400

    Dynamically load plot libs

--- wallet/settings.py
+++ wallet/settings.py
@@ -95,8 +95,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.gzip.GZipMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
